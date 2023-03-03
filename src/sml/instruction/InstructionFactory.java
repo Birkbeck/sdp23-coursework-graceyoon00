e9272255484ff59b4ebb73cc4ff8c9f92de64df0
Grace <gyoon18@bu.edu>
2023-03-03 16:55:17 +0000

    Working on InstructionFactory

--- src/sml/instruction/InstructionFactory.java
+++ src/sml/instruction/InstructionFactory.java
@@ -1,17 +1,22 @@
 package sml.instruction;
 
 import sml.Instruction;
+import sml.RegisterName;
+
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class InstructionFactory {
 
-    public InstructionFactory(){
-    }
+    public Instruction getInstance(String label, RegisterName result, RegisterName source) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
 
-    public Instruction getInstance(String label) {
+        Constructor<?> constructor = null;
+
         if (label.equals("add")) {
-
+            constructor = AddInstruction.class.getConstructor(String.class, RegisterName.class, RegisterName.class);
         }
+
+        return (Instruction) constructor.newInstance(label, result, source);
     }
 
 
